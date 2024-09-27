740e5e23c03c6023764b529071e094d9e941ce78
yuanyxh <15766118362@139.com>
2024-09-27 23:37:40 +0800

    feat: chagne

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { basicSetup, EditorView } from "codemirror";
+import { markdown } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { useEffect, useRef } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const divRef = useRef<HTMLDivElement>(null);
+  const viewRef = useRef<EditorView>();
+
+  useEffect(() => {
+    viewRef.current = new EditorView({
+      doc: "Hello\n\n```javascript\nlet x = 'y'\n```",
+      extensions: [basicSetup, markdown({ codeLanguages: languages })],
+      parent: divRef.current!,
+    });
+
+    return () => {
+      viewRef.current?.destroy();
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={styles.container}>
+      <div ref={divRef} className={styles.editor}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
